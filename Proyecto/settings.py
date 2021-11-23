c642879551cd298fe8a8ec9f4bbed02387be6bd8
ScarlettOC <94888415+ScarlettOC@users.noreply.github.com>
2021-11-23 03:45:43 -0600

    Update settings.py

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -137,4 +137,4 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/crud/listar'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
